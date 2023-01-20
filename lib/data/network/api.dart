15d9c14808c039cce35d4a489d048e0c3e8a9f43
fauzirramadan <fauzirramadan@gmail.com>
2023-01-20 23:27:47 +0800

    add prefs utils

--- lib/data/network/api.dart
+++ lib/data/network/api.dart
@@ -1,1 +1,10 @@
-class Api {}
+import 'package:dio/dio.dart';
+
+class Api {
+  static final authorization = Options(headers: {
+    "Authorization": "VpKonmdWMy3iMNtsFOZltVSQsjm4XOctpdrWT29df6PLROCAwZMd2rMw"
+  });
+
+  static const String baseUrl = "https://api.pexels.com/v1";
+  static const String photoApi = "$baseUrl/curated";
+}
